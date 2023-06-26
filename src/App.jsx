478105a7ec71fd7f4c3b0bfb562e84430328d6f8
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-06-26 22:09:17 +0800

    Latest Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
 
 function App() {
 
   return (
     <>
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-9xl text-center font-bold text-teal-500">Hello World!</h1>
-      <h2 className='text-7xl text-teal-900'>Yehey Tailwind Framework</h2>
-    </div>
+    <main className='bg-sky-200 px-10'>
+      <section className='min-h-screen'>
+        <nav className='py-10 mb-12 flex justify-between'>
+          <h1 className='
+          text-2xl font-bold'>Ryoma</h1>
+          <ul>
+            <li><BsFillMoonStarsFill className='cursor-pointer text-2xl' /></li>
+            <li><a href="#" className='bg-teal-500 py-2 px-4 rounded-lg'>Resume</a></li>
+          </ul>
+        </nav>
+      </section>
+    </main>
         
     </>
   )
